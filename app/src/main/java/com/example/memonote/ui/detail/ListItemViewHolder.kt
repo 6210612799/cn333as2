068f4964a3e03372d811e1e991c760b8619f6787
Mc Punyawat <6210612799@student.tu.ac.th>
2022-02-24 05:22:45 +0700

    final

---
+++ app/src/main/java/com/example/memonote/ui/detail/ListItemViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.memonote.ui.detail
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.memonote.databinding.ListItemViewHolderBinding
+
+class ListItemViewHolder(val binding: ListItemViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
+
+}
---
+++ app/src/main/java/com/example/memonote/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.memonote.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.memonote.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding):
+    RecyclerView.ViewHolder(binding.root) {
+
+}
+
+/* class A extends B {
+    private int x;
+    public A(int x) {
+       this.x = x
+
+       }
+}
+
+class A(x: Int): B(x) {}
+ */
